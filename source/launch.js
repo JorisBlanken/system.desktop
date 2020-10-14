685953ee78a8a5eb983821f5ec9b7867f68630fb
Joris Blanken <jorisblanken@gmail.com>
2020-10-14 12:54:21 -0500

    add desktop to each screen device

--- source/launch.js
+++ source/launch.js
@@ -3,19 +3,21 @@ import { app, BrowserWindow, screen } from 'electron';
 app.whenReady().then(launch);
 
 async function launch() {
-    let primary = screen.getPrimaryDisplay();
-
-    let window = new BrowserWindow({
-        x: primary.bounds.x,
-        y: primary.bounds.y,
-        width: primary.bounds.width,
-        height: primary.bounds.height,
-        show: false,
-        frame: false,
-        focusable: false,
-        type: 'desktop'
-    });
-
-    window.loadFile('./source/interface/desktop.html');
-    window.once('ready-to-show', () => window.show());
+    let displays = screen.getAllDisplays();
+    
+    for (let display of displays) {
+        let {x, y, width, height} = display.bounds;
+        let settings = {
+            show: false,
+            frame: false,
+            focusable: false,
+            type: 'desktop',
+            title: 'desktop'
+        }
+    
+        let window = new BrowserWindow({ ...settings, x, y, width, height });
+    
+        window.loadFile('./source/interface/desktop.html');
+        window.once('ready-to-show', () => window.show());
+    }
 }
